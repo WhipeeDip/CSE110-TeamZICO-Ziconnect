e1d2891d842320b8b1f7bfaf7f18949fddcdb776
Asianu <l6leung@ucsd.edu>
2017-03-05 21:42:08 -0800

    Fixed navbar, ready for use
    -make sure to load angular, angular-route
    -make sure to load jquery and bootstrap
    
    -make sure to load <script src=“../directives/navbar.js”></script>

--- public/directives/navbar.js
+++ public/directives/navbar.js
@@ -2,6 +2,6 @@ angular.module('navigate', ['ngRoute'])
     .directive('navbar', function () {
         'use strict';
         return {
-            templateUrl: "navbar.html"
+            templateUrl: "../directives/navbar.html"
         };
     });
