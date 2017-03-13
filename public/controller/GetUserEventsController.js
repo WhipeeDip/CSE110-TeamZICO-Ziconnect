3287435d943a271c5d34a14250f7290061e244eb
Asianu <l6leung@ucsd.edu>
2017-03-13 13:34:31 -0700

    Sidebar issues
    -Cannot use “user.uid” to pass into angular function
    -However, {{user.uid}} in html successfully retrieves user’s id

--- public/controller/GetUserEventsController.js
+++ public/controller/GetUserEventsController.js
@@ -8,7 +8,21 @@
 angular.module('controllers')
   .controller('SidebarController', ['$scope', '$firebaseArray',
     function($scope, $firebaseArray){
+      
+      $scope.getUser = function(){
+        $scope.user = AccountServices.getUser();
+      }
+      
+      // grabs the events that the user is in by filtering through:
+      //    eventsUserIsIn
+      //    eventList
+      $scope.getUserEvents = function(uid){
+        console.log("its working not really " + uid);
+        var eventRef = firebase.database().ref('eventList');
+        var eUserIsInRef = firebase.database().ref('eventsUserIsIn').child(uid);
         
+      }
+      
       var eventRef = firebase.database().ref('eventList');
       var list = $firebaseArray(eventRef);
       $scope.list = list;
