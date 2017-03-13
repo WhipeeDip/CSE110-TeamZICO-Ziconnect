464af25264c9df8f3201aceaa3b7b5555077fe1a
Elliot Yoon <elliotyoon@outlook.com>
2017-03-13 06:47:51 -0700

    Comments are now recorded on Firebase, but aren't being shown. Also dates aren't being saved.

--- public/controller/CommentController.js
+++ public/controller/CommentController.js
@@ -5,10 +5,17 @@
  */
 
 angular.module('controllers')
-  .controller('CommentController', ['CommentServices', '$scope', '$firebaseArray',
-    function(CommentServices, $scope, $firebaseArray) {
+  .controller('CommentController', ['CommentServices', '$scope', '$rootScope', '$firebaseArray',
+    function(CommentServices, $scope, $rootScope, $firebaseArray) {
+      var eventUid = $scope.eventData.$id;
+
       var commentRef = firebase.database().ref().child('eventComments/' + eventUid);
       var commentList = $firebaseArray(commentRef);
 
+      $scope.postComment = function() {
+        var date = new Date();
+        console.log('Date:', date);
+        CommentServices.addComment(eventUid, $rootScope.user.uid, $scope.commentText, date);
+      };
     }
   ]);
