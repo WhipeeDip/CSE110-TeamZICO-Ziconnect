9f92cec67124483891da23ef8782997b618187a9
Caris Wei <cariswei@gmail.com>
2017-03-12 23:08:24 -0700

    Almost done with searching users for invite. Going to McDonalds should finish it soon

--- public/controller/InviteController.js
+++ public/controller/InviteController.js
@@ -1,17 +1,30 @@
 /**
- * File name: GetUsersEventsController.js
- * Authors: Justin Cai
- * Description: Controller for getting user's events in sidebar.
+ * File name: InviteController.js
+ * Authors: Justin Cai, Caris Wei
+ * Description: Controller for adding people to event
  */
-
 
 angular.module('controllers')
   .controller('InviteController', ['$scope', '$firebaseArray',
     function($scope, $firebaseArray){
+      var userRef = firebase.database().ref("userList");
+      console.log(userRef);
+      $scope.input = null;
         
-      var eventRef = firebase.database().ref('userList');
-      var list = $firebaseArray(eventRef);
-      $scope.list = list;
-
+      $scope.inviteFunc = function(){
+          
+        var list = $firebaseArray(           userRef);
+        $scope.list = list;
+        console.log($scope.input);
+          
+        var res = [];
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].name.toLowerCase().search(($scope.input).toLowerCase) != -1) {
+                res.push(list[i]);
+            }
+        }
+          console.log("res");
+          console.log(res);
+      }
     }
   ]);
