339f24a5f6936c5572ffe157041fdf691f37969a
kevany23 <kevan23yang@gmail.com>
2017-03-15 00:01:44 -0700

    Added potluck food

--- public/controller/FoodController.js
+++ public/controller/FoodController.js
@@ -4,12 +4,28 @@
  * Description: Handles food to be brought to event, if potluck mode
  */
 
-var foodcrl = angular.module('ziconnect');
-foodctrl.controller('foodController', ['$scope', function($scope) {
-  $scope.storeFood = function(name, uid, food) {
+ var foodctrl = angular.module('controllers');
+ foodctrl.controller('FoodController', ['$scope', '$rootScope', '$firebaseArray',
+ function($scope, $rootScope, $firebaseArray) {
 
-  };
-  $scope.getList = function() {
+   var eventUid = $scope.eventData.$id;
 
-  }
-}]);
+   var potluckRef = firebase.database().ref().child('potluck/' + eventUid);
+   $scope.foodList = $firebaseArray(potluckRef);
+
+   //$scope.foodList = [];
+   // should be [food, uid]
+   var foodPair = [];
+   $scope.storeFood = function(name, uid, food) {
+     console.log('food:' + food);
+     var key = potluckRef.push();
+     potluckRef.push({
+       'UID': uid,
+       'name': name,
+       'food': food
+     });
+
+   };
+   $scope.getList = function() {
+   }
+ }]);
