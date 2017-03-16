f7e16556de4e83ac191dd0ce20f007f852f5080d
kevany23 <kevan23yang@gmail.com>
2017-03-16 00:57:20 -0700

    Fixed group services

--- public/models/GroupServices.js
+++ public/models/GroupServices.js
@@ -7,12 +7,8 @@
 angular.module('models')
   .factory('GroupServices', ['$q',
     function($q) {
-      return {
-        // creates a new group
-        createGroup: function(creatorUid) {
-          var deferred = $q.defer();
+     return {
 
-<<<<<<< HEAD
       // creates a new group
       createGroup: function(creatorUid, name) {
         var deferred = $q.defer();
@@ -31,46 +27,29 @@ angular.module('models')
           newGroupUserRef.set(true).then(function() {
             console.log('New group created:', groupRef.key);
             deferred.resolve();
-=======
-          var groupListsRef = firebase.database().ref().child('groupList');
-          var groupRef = groupListsRef.push({'creator': creatorUid});
-          groupRef.then(function() {
-            var groupUid = groupRef.key;
-            var groupsUserIsInRef = firebase.database().ref().child('groupsUserIsIn/' + creatorUid);
-            var newGroupUserRef = groupsUserIsInRef.child(groupUid);
-            newGroupUserRef.set(true).then(function() {
-              console.log('New group created:', groupRef.key);
-              deferred.resolve();
-            });
->>>>>>> 852efdc2dda8250579d8b6ab17533426105cd45a
           });
+        })
 
-          return deferred.promise;
-        },
+        return deferred.promise;
+      },
 
-        // adds a user to a group
-        addUserToGroup: function(groupUid, userUid) {
-          var deferred = $q.defer();
+      // adds a user to a group
+      addUserToGroup: function(groupUid, userUid) {
+        var deferred = $q.defer();
 
-          var groupRef = firebase.database().ref().child('groupList/' + groupUid);
-          var newUserRef = groupRef.child(userUid);
-          newUserRef.set(true).then(function() {
-            var groupsUserIsInRef = firebase.database().ref().child('groupsUserIsIn/' + userUid + '/' + groupUid);
-            groupsUserIsInRef.set(true).then(function() {
-              console.log('User ', userUid, ' added to group ', groupUid);
-              deferred.resolve();
-            });
+        var groupRef = firebase.database().ref().child('groupList/' + groupUid);
+        var newUserRef = groupRef.child(userUid);
+        newUserRef.set(true).then(function() {
+          var groupsUserIsInRef = firebase.database().ref().child('groupsUserIsIn/' + userUid + '/' + groupUid);
+          groupsUserIsInRef.set(true).then(function() {
+            console.log('User ', userUid, ' added to group ', groupUid);
+            deferred.resolve();
           });
+        });
 
-<<<<<<< HEAD
         return deferred.promise;
       }
 
      }
-=======
-          return deferred.promise;
-        }
-     } 
->>>>>>> 852efdc2dda8250579d8b6ab17533426105cd45a
     }
   ]);
